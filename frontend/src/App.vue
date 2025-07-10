bd57cb3f81ea0cefcb0a244cb3e0d37bd71b1dd5
inxx <noobish.in@gmail.com>
2025-07-10 09:20:06 +0900

    Remove unnecessary template components and styles from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import InputToken from './components/InputToken.vue'
 </script>
 
@@ -9,31 +7,4 @@ import InputToken from './components/InputToken.vue'
   <InputToken />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
